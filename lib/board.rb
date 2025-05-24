8d08be11d54649f6fa201dc5cdf6aca69b8d22a0
SamrudhNelli <samrudhnelli@gmail.com>
2025-05-24 11:22:09 +0530

    Added colors to the board
    The coloring support was added using the gem 'colorize'.

--- lib/board.rb
+++ lib/board.rb
@@ -1,11 +1,16 @@
+require 'colorize'
+
+String.prevent_colors = true  # don't colorize colorized strings
+String.prevent_colors true    # don't colorize colorized strings
+
 module Board
   def self.print(array)
-    puts "\n           ***************************************************************************      \n\n"
-    puts "                                         #{(array[0].nil?)? " ":array[0]} | #{(array[1].nil?)? " ":array[1]} | #{(array[2].nil?)? " ":array[2]}  \n"
-    puts "                                      --------------- \n"
-    puts "                                         #{(array[3].nil?)? " ":array[3]} | #{(array[4].nil?)? " ":array[4]} | #{(array[5].nil?)? " ":array[5]}  \n"
-    puts "                                      --------------- \n"
-    puts "                                         #{(array[6].nil?)? " ":array[6]} | #{(array[7].nil?)? " ":array[7]} | #{(array[8].nil?)? " ":array[8]}  \n"
-    puts "\n           ***************************************************************************      \n\n"
+    puts "\n           ***************************************************************************      \n".colorize(:light_green)
+    puts "                                         #{(array[0].nil?)? " " :(array[0].eql?('X'))? array[0].colorize(:light_red): array[0].colorize(:light_blue)} | #{(array[1].nil?)? " ":(array[1].eql?('X'))? array[1].colorize(:light_red): array[1].colorize(:light_blue)} | #{(array[2].nil?)? " ":(array[2].eql?('X'))? array[2].colorize(:light_red): array[2].colorize(:light_blue)}  ".colorize(:light_green)
+    puts "                                      --------------- ".colorize(:light_green)
+    puts "                                         #{(array[3].nil?)? " " :(array[3].eql?('X'))? array[3].colorize(:light_red): array[3].colorize(:light_blue)} | #{(array[4].nil?)? " ":(array[4].eql?('X'))? array[4].colorize(:light_red): array[4].colorize(:light_blue)} | #{(array[5].nil?)? " ":(array[5].eql?('X'))? array[5].colorize(:light_red): array[5].colorize(:light_blue)}  ".colorize(:light_green)
+    puts "                                      --------------- ".colorize(:light_green)
+    puts "                                         #{(array[6].nil?)? " " :(array[6].eql?('X'))? array[6].colorize(:light_red): array[6].colorize(:light_blue)} | #{(array[7].nil?)? " ":(array[7].eql?('X'))? array[7].colorize(:light_red): array[7].colorize(:light_blue)} | #{(array[8].nil?)? " ":(array[8].eql?('X'))? array[8].colorize(:light_red): array[8].colorize(:light_blue)}  ".colorize(:light_green)
+    puts "\n           ***************************************************************************      \n".colorize(:light_green)
   end
 end
