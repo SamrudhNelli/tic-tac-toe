c3ed41b672b12244645fa5d7b04e548a9b5ecc99
SamrudhNelli <samrudhnelli@gmail.com>
2025-05-25 08:19:20 +0530

    Update the code readability
    Rubocup was used to update the readability of the code
    Also ASCII Text Art Generator by fsymbols was used to generate a text art.

--- lib/board.rb
+++ lib/board.rb
@@ -1,16 +1,16 @@
-require 'colorize'
+require "colorize"
 
 String.prevent_colors = true  # don't colorize colorized strings
 String.prevent_colors true    # don't colorize colorized strings
 
 module Board
   def self.print(array)
-    puts "\n           ***************************************************************************      \n".colorize(:light_green)
-    puts "                                         #{(array[0].nil?)? " " :(array[0].eql?('X'))? array[0].colorize(:light_red): array[0].colorize(:light_blue)} | #{(array[1].nil?)? " ":(array[1].eql?('X'))? array[1].colorize(:light_red): array[1].colorize(:light_blue)} | #{(array[2].nil?)? " ":(array[2].eql?('X'))? array[2].colorize(:light_red): array[2].colorize(:light_blue)}  ".colorize(:light_green)
-    puts "                                      --------------- ".colorize(:light_green)
-    puts "                                         #{(array[3].nil?)? " " :(array[3].eql?('X'))? array[3].colorize(:light_red): array[3].colorize(:light_blue)} | #{(array[4].nil?)? " ":(array[4].eql?('X'))? array[4].colorize(:light_red): array[4].colorize(:light_blue)} | #{(array[5].nil?)? " ":(array[5].eql?('X'))? array[5].colorize(:light_red): array[5].colorize(:light_blue)}  ".colorize(:light_green)
-    puts "                                      --------------- ".colorize(:light_green)
-    puts "                                         #{(array[6].nil?)? " " :(array[6].eql?('X'))? array[6].colorize(:light_red): array[6].colorize(:light_blue)} | #{(array[7].nil?)? " ":(array[7].eql?('X'))? array[7].colorize(:light_red): array[7].colorize(:light_blue)} | #{(array[8].nil?)? " ":(array[8].eql?('X'))? array[8].colorize(:light_red): array[8].colorize(:light_blue)}  ".colorize(:light_green)
-    puts "\n           ***************************************************************************      \n".colorize(:light_green)
+    puts "\n***************************************************************************      \n".colorize(:light_green)
+    puts "                            #{array[0].nil? ? ' ' : array[0].eql?('X') ? array[0].colorize(:light_red) : array[0].colorize(:light_blue)} | #{array[1].nil? ? ' ' : array[1].eql?('X') ? array[1].colorize(:light_red) : array[1].colorize(:light_blue)} | #{array[2].nil? ? ' ' : array[2].eql?('X') ? array[2].colorize(:light_red) : array[2].colorize(:light_blue)}  ".colorize(:light_green)
+    puts "            --------------- ".colorize(:light_green)
+    puts "                            #{array[3].nil? ? ' ' : array[3].eql?('X') ? array[3].colorize(:light_red) : array[3].colorize(:light_blue)} | #{array[4].nil? ? ' ' : array[4].eql?('X') ? array[4].colorize(:light_red) : array[4].colorize(:light_blue)} | #{array[5].nil? ? ' ' : array[5].eql?('X') ? array[5].colorize(:light_red) : array[5].colorize(:light_blue)}  ".colorize(:light_green)
+    puts "             --------------- ".colorize(:light_green)
+    puts "                            #{array[6].nil? ? ' ' : array[6].eql?('X') ? array[6].colorize(:light_red) : array[6].colorize(:light_blue)} | #{array[7].nil? ? ' ' : array[7].eql?('X') ? array[7].colorize(:light_red) : array[7].colorize(:light_blue)} | #{array[8].nil? ? ' ' : array[8].eql?('X') ? array[8].colorize(:light_red) : array[8].colorize(:light_blue)}  ".colorize(:light_green)
+    puts "\n***************************************************************************      \n".colorize(:light_green)
   end
 end
--- main.rb
+++ main.rb
@@ -1,14 +1,21 @@
-require_relative "lib/board" 
+require_relative "lib/board"
 require_relative "lib/game"
 require_relative "lib/win?"
 
 def play_game(num, player1, player2)
   array = Array.new(11, nil)
-  array[9] = if num.odd? then 0 else 1 end
-  array[10] = 'X'
-  until(win?.stop(array))
-    puts "This is #{if array[9].odd? then player2 else player1 end}'s turn\n"
+  array[9] = (num.odd? ? 0 : 1)
+  array[10] = "X"
+  until win?.stop(array)
+    puts "This is #{array[9].odd? ? player2 : player1}'s turn\n"
     Board.print(array)
     array = Game.play(array)
   end
 end
+
+puts " ████████╗██╗░█████╗░  ░░░░░░  ████████╗░█████╗░░█████╗░  ░░░░░░  ████████╗░█████╗░███████╗"
+puts " ╚══██╔══╝██║██╔══██╗  ░░░░░░  ╚══██╔══╝██╔══██╗██╔══██╗  ░░░░░░  ╚══██╔══╝██╔══██╗██╔════╝"
+puts " ░░░██║░░░██║██║░░╚═╝  █████╗  ░░░██║░░░███████║██║░░╚═╝  █████╗  ░░░██║░░░██║░░██║█████╗░░"
+puts " ░░░██║░░░██║██║░░██╗  ╚════╝  ░░░██║░░░██╔══██║██║░░██╗  ╚════╝  ░░░██║░░░██║░░██║██╔══╝░░"
+puts " ░░░██║░░░██║╚█████╔╝  ░░░░░░  ░░░██║░░░██║░░██║╚█████╔╝  ░░░░░░  ░░░██║░░░╚█████╔╝███████╗"
+puts " ░░░╚═╝░░░╚═╝░╚════╝░  ░░░░░░  ░░░╚═╝░░░╚═╝░░╚═╝░╚════╝░  ░░░░░░  ░░░╚═╝░░░░╚════╝░╚══════╝"
