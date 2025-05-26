f64e28946094fd8a962e077b341a5979c5696395
SamrudhNelli <samrudhnelli@gmail.com>
2025-05-26 08:50:15 +0530

    This is the FINAL version of the game
    All the modules were clubbed together in main.rb.
    Some bugs were rectified to successfully run the game.

--- lib/game.rb
+++ lib/game.rb
@@ -2,12 +2,17 @@ module Game
   def self.play(array)
     print "Play your move (Available boxes : "
     (0...array.length).each do |i|
-      print "#{i + 1} " if array[i].nil?
+      print "#{i} " if array[i].nil?
     end
     print ")\nEnter here : "
     input = gets.chomp
-    array[input.to_i - 1] = array[10]
+    if array[input.to_i].nil? == false || input.to_i.negative? || input.to_i > 8
+      puts "Invalid input, please try again.\n".colorize(:red)
+      return play(array)
+    end
+    array[input.to_i] = array[10]
     array[10] = array[10].eql?("X") ? "O" : "X"
+    array[9] = (array[9].odd? ? 0 : 1)
     array
   end
 end
--- main.rb
+++ main.rb
@@ -6,16 +6,37 @@ def play_game(num, player1, player2)
   array = Array.new(11, nil)
   array[9] = (num.odd? ? 0 : 1)
   array[10] = "X"
-  until win?.stop(array)
-    puts "This is #{array[9].odd? ? player2 : player1}'s turn\n"
-    Board.print(array)
-    array = Game.play(array)
+  until Win.stop(array)
+    puts "\n\nThis is #{array[9].odd? ? player1 : player2}'s turn\n".colorize(:cyan)
+    Board.print array
+    array = Game.play array
   end
+  Board.print array
+  Win.print_result array, player1, player2
 end
 
-puts " ████████╗██╗░█████╗░  ░░░░░░  ████████╗░█████╗░░█████╗░  ░░░░░░  ████████╗░█████╗░███████╗"
-puts " ╚══██╔══╝██║██╔══██╗  ░░░░░░  ╚══██╔══╝██╔══██╗██╔══██╗  ░░░░░░  ╚══██╔══╝██╔══██╗██╔════╝"
-puts " ░░░██║░░░██║██║░░╚═╝  █████╗  ░░░██║░░░███████║██║░░╚═╝  █████╗  ░░░██║░░░██║░░██║█████╗░░"
-puts " ░░░██║░░░██║██║░░██╗  ╚════╝  ░░░██║░░░██╔══██║██║░░██╗  ╚════╝  ░░░██║░░░██║░░██║██╔══╝░░"
-puts " ░░░██║░░░██║╚█████╔╝  ░░░░░░  ░░░██║░░░██║░░██║╚█████╔╝  ░░░░░░  ░░░██║░░░╚█████╔╝███████╗"
-puts " ░░░╚═╝░░░╚═╝░╚════╝░  ░░░░░░  ░░░╚═╝░░░╚═╝░░╚═╝░╚════╝░  ░░░░░░  ░░░╚═╝░░░░╚════╝░╚══════╝"
+puts "\n████████╗██╗░█████╗░  ░░░░░░  ████████╗░█████╗░░█████╗░  ░░░░░░  ████████╗░█████╗░███████╗".colorize(:cyan)
+puts " ╚══██╔══╝██║██╔══██╗  ░░░░░░  ╚══██╔══╝██╔══██╗██╔══██╗  ░░░░░░  ╚══██╔══╝██╔══██╗██╔════╝".colorize(:cyan)
+puts " ░░░██║░░░██║██║░░╚═╝  █████╗  ░░░██║░░░███████║██║░░╚═╝  █████╗  ░░░██║░░░██║░░██║█████╗░░".colorize(:cyan)
+puts " ░░░██║░░░██║██║░░██╗  ╚════╝  ░░░██║░░░██╔══██║██║░░██╗  ╚════╝  ░░░██║░░░██║░░██║██╔══╝░░".colorize(:cyan)
+puts " ░░░██║░░░██║╚█████╔╝  ░░░░░░  ░░░██║░░░██║░░██║╚█████╔╝  ░░░░░░  ░░░██║░░░╚█████╔╝███████╗".colorize(:cyan)
+puts " ░░░╚═╝░░░╚═╝░╚════╝░  ░░░░░░  ░░░╚═╝░░░╚═╝░░╚═╝░╚════╝░  ░░░░░░  ░░░╚═╝░░░░╚════╝░╚══════╝".colorize(:cyan)
+
+print "\n\nWelcome to the game of TIC-TAC-TOE\nEnter your names here,\n\nPlayer 1 : ".colorize(:cyan)
+name1 = gets.chomp
+print "\nPlayer 2 : ".colorize(:cyan)
+name2 = gets.chomp
+print "\nBasic info regarding the game\n".colorize(:cyan)
+Board.init
+
+continue_game = true
+count = 0
+
+while continue_game
+  play_game count, name1, name2
+  puts "\nDo you want to play once again? (Y/n)".colorize(:cyan)
+  print "Your choice : "
+  choice = gets.chomp
+  continue_game = false if choice.upcase == "N"
+  count += 1
+end
